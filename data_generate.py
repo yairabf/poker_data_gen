33650200f006e2781d886a855818c8352a08df4c
yair.abramovitch <yairabc@gmail.com>
2018-06-15 18:28:06 +0300

    finished the beta

--- data_generate.py
+++ data_generate.py
@@ -1,20 +1,25 @@
+from __future__ import division
 import random as rand
 import numpy as np
 
 data = open('poker-hand-testing.data', 'rw')
-data_line = data.readlines()\
+new_data = open('new_data.txt', 'w')
+data_line = data.readlines()
 
 for i in range(25000):
     plays_first = rand.getrandbits(1)
     pot_amount = rand.randint(1000, 100000)
     player_amount = rand.randint(1000, 10000)
     bet = rand.randint(1, player_amount)
-    bet_relate_pot = bet/pot_amount
-    bet_relate_equity = bet/player_amount
+    if plays_first == 1:
+        bet_relate_pot = 0
+    else:
+        bet_relate_pot = bet / pot_amount
+    bet_relate_equity = bet / player_amount
     number_of_hands = i
     hand = data_line[i]
     instance = str(plays_first) + ',' + str(round(bet_relate_pot, 2)) + ',' + str(round(bet_relate_equity, 2)) + ',' + \
-               str(number_of_hands) + ',' + hand[len(hand)-3]
-    print instance
+               str(number_of_hands) + ',' + hand[len(hand)-3]+'\n'
+    new_data.write(instance)
 print "end"
 
